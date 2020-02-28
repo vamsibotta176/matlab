b93471497ddd59e165993d2ac75ebd30cc12f6f7
vamsibotta176 <61054254+vamsibotta176@users.noreply.github.com>
2020-02-28 12:43:35 -0500

    Add files via upload

---
+++ patterns.m
@@ -0,0 +1,7 @@
+rows=4
+for i =1:rows
+    for j=1:i
+        fprintf('*')
+    end
+    fprintf('\n')
+end
---
+++ stars.m
@@ -0,0 +1,8 @@
+row=10
+columns=67
+for i =1:row
+    for j=1:columns
+        fprintf('*')
+    end
+    fprintf('\n')
+end
